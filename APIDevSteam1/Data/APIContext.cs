b57291b87560a4c7d5094bf165db076262e1b929
limazxz <goubulsegue@gmail.com>
2025-04-16 11:57:35 -0300

    Adicionar arquivos de projeto.

---
+++ APIDevSteam1/Data/APIContext.cs
@@ -0,0 +1,22 @@
+﻿using APIDevSteamJau.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIDevSteam1.Data
+{
+    public class APIContext : IdentityDbContext<Usuario>
+    {
+        public APIContext(DbContextOptions<APIContext> options) : base(options)
+        {
+        }
+
+        //Dbset
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // tabelas
+
+        }
+    }
+}
